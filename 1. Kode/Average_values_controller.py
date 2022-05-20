4547f5e99028ff6ebd17c319671d62282feb33a8
Dawidli <dawid.lit32@gmail.com>
2022-05-20 12:02:37 +0200

    Update Average_values_controller.py

--- 1. Kode/Average_values_controller.py
+++ 1. Kode/Average_values_controller.py
@@ -9,35 +9,25 @@ y = [0]*10 # tom y[]
 x_hastighet = [0]*10
 y_hastighet = [0]*10
 
+global counter
+y = [0]*10 # tom y[]
 fps = 30
 sample_time = 1/fps # fps
 counter = 0
 grense = len(x)
-x_verdier = np.linspace(0,1000,fps*5)
-y_verdier = np.linspace(0,500,fps*5)
+
 
 
 def find_average(matrise, input):
     global counter
+
     tot = 0
+    counter = counter % grense
+    matrise[counter] = input
+    for I in range(len(matrise)):
+        tot += matrise[I]
+    avg = tot/len(matrise)
+    print(avg)
+    counter += 1
 
-    for i in range(fps*5):
-        counter = counter % grense
-        matrise[counter] = round(input[i])
-        for I in range(len(matrise)):
-            tot += matrise[I]
-        avg = tot/len(matrise)
-        print(avg)
-        counter += 1
-
-def find_hastighet(current, avg, speed_avg):
-    global counter
-
-    d = 17.5
-    speed = avg/sample_time
-    speed[counter] = speed
-
-
-x_avg = find_average(x, x_verdier)
-find_hastighet(x_verdier, x_avg, x_hastighet)
 
