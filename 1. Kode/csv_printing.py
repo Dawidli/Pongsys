1471a77b8a3f142480f8c85944ce021ee411b74f
Dawidli <dawid.lit32@gmail.com>
2022-05-22 16:30:12 +0200

    Create csv_printing.py

---
+++ 1. Kode/csv_printing.py
@@ -0,0 +1,10 @@
+import csv
+
+x = ["posx,", "posy"]
+
+with open('eggs.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for i in range(10):
+        spamwriter.writerow(x)
+#
