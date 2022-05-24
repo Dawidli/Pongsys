cffa7bfb7f20bde2f1a1778928c7fe46ce2acc34
Simeskaa <81254666+Simeskaa@users.noreply.github.com>
2022-05-24 21:02:16 +0200

    commit die

--- 1. Kode/perfect_everything_works.py
+++ 1. Kode/perfect_everything_works.py
@@ -10,32 +10,43 @@ from tkinter import *
 import imutils
 import time
 
+# -------------------------------------------Both programs(Servo Control and Ball Tracker) in one -------------------------------------------
+"""
+For running both programs simultaneously we can use multithreading or multiprocessing
+"""
 
 #define servo angles and set a value
 servo1_angle = 0
 servo2_angle = 0
 servo3_angle = -6.3
 all_angle = 0
+# Set a limit to upto which you want to rotate the servos (You can do it according to your needs)
 
-time_array = [time.time()]*2
+
+"FOR LAVPASS FILTER / BÅNDBEGRENSET DERIVASJONS FILTER KAN VI GÅ PÅ 12_DTFT OG PRAKSIS FILTER DESIGN"
 
 platform_angle = 0 # intial
 delta_t = 1 / 100
 velocity = [0] # initial verdi
 pos_x = [0, 0]
 pos_y = [0, 0]
-K = [0.27, 0.002285] #very cool stabiliet
+#K = [0.57, 0.0045885] #very cool stabiliet
+K = [0.27, 0.00228] # kritisk stabilt
 
-x = [0.0]*4 # tom y[]
-y = [0.0]*4 # tom y[]
-x_avg = [0.0]*4
-y_avg = [0.0]*4
+
+
+
+global counter
+x = [0.0]*10 # tom y[]
+x_avg = [0.0]*10
+y = [0.0]*10 # tom y[]
+y_avg = [0.0]*10
 speed = [0.0]*2
+fps = 20
+sample_time = 1/fps # fps
 counter = 0
 grense = len(x)
 distance_error = [0.0, 0.0]
-fps = 30
-sample_time = 1/fps # fps
 
 
 def ball_track(key1, queue):
@@ -87,34 +98,33 @@ def ball_track(key1, queue):
         cv2.waitKey(1)
 
 
+
 def servo_control(key2, queue):
+    time_array = [time.time()]*2
+    no_sleep = 100
     port_id = 'COM3'
     # initialise serial interface
     arduino = serial.Serial(port=port_id, baudrate=250000, timeout=0.1)
     if key2:
         print('Servo controls are initiated')
 
-    def find_average(x_matrise, y_matrise, x_cord, y_cord, x_avg, y_avg):
+    def find_average(matrise, input, avg):
         global counter
-        x_tot = 0.0
-        y_tot = 0.0
+
+        tot = 0.0
         counter = counter % grense
-        x_matrise[counter] = x_cord
-        y_matrise[counter] = y_cord
-        for I in range(len(x_matrise)):
-            x_tot += x_matrise[I]
-            y_tot += y_matrise[I]
-        x_avg[counter] = (x_tot / len(x_matrise)) * 1.277
-        y_avg[counter] = (y_tot / len(y_matrise)) * 1.277
+        matrise[counter] = input
+        for I in range(len(matrise)):
+            tot += matrise[I]
+        avg[counter] = (tot / len(matrise)) * 1.277
+        # print(avg)
+        counter += 1
 
-
-    def find_speed(avg):
-        global counter
-        speed = (avg[counter] - avg[counter]) / (sample_time)
-
+    def find_speed(avg, fix):
+        speed = (avg[counter - 1 - fix] - avg[counter - 3 - fix]) / (sample_time)
         return speed
 
-    def all_angle_assign(angle_passed1, angle_passed2, angle_passed3):
+    def all_angle_assign(angle_passed1,angle_passed2,angle_passed3):
         global servo1_angle, servo2_angle, servo3_angle
         servo1_angle = float(angle_passed1)
         servo2_angle = float(angle_passed2)
@@ -124,45 +134,75 @@ def servo_control(key2, queue):
     root = Tk()
     root.resizable(0, 0)
 
+    def Preg(xverdi, yverdi):
+        global servo_values
+
+        if xverdi == 'n' and yverdi == 'i':
+            return
+        else:
+            Preg_values = [0] * 2
+
+            for i in range(2):
+                dist = yverdi if i == 0 else xverdi
+                cord_ratio = dist / 28
+                d = 17.5 if i == 0 else 20
+                platform_angle = math.radians((cord_ratio * 105) / (7))
+                motor_angle = np.arcsin((d * np.sin(platform_angle)) / (2 * 4))
+                Preg_values[i] = -motor_angle # indeks 0 er pitch og indeks 1 er roll
+            servo_values = [Preg_values[0] - Preg_values[1], Preg_values[0] + Preg_values[1], -Preg_values[0]]
+            #print("Servo 1: ",round(servo_values[0],3),"Servo 2: ",round(servo_values[1],3),"Servo 3: ",round(servo_values[2],3))
+        # have to fix a min/max regulation for servo_values ;)
+
     def PDreg(x_error, y_error):
         global servo_values, pos_y, pos_x, distance_error, K, speed, counter, platform_angle
+
 
         Regulator_values = [0] * 2
         distance_error[1] = y_error[counter - 1]
         distance_error[0] = x_error[counter - 2]
         for i in range(2):
+
             d = 20.2 if i == 0 else 17.5
 
             # regulator
-            platform_angle = math.radians(-K[0] * distance_error[i]) - (K[1] * speed[i - 1])
-            # print(platform_angle)
+            platform_angle = math.radians(-K[0] * distance_error[i]) - (K[1] * speed[i-1])
+            #print(platform_angle)
             # converts platform angle to servo angles and sends away
-            motor_angle = np.arcsin((d * np.sin(platform_angle)) / (2 * 4))
+            motor_angle = np.arcsin((d * np.sin(platform_angle)) / (2*4))
             Regulator_values[i] = motor_angle  # indeks 0 er pitch og indeks 1 er roll
-        servo_values = [-Regulator_values[0] + (0.549 * Regulator_values[1]),
-                        Regulator_values[0] + (0.549 * Regulator_values[1]),
-                        -Regulator_values[1]]
+        servo_values = [-Regulator_values[0] + (0.549 * Regulator_values[1]), Regulator_values[0] + (0.549 * Regulator_values[1]),
+                            -Regulator_values[1]]
+
 
     def writeCoord():
-        global counter
+        """
+        Here in this function we get both coordinate and servo control, it is an ideal place to implement the controller
+        """
         corrd_info = queue.get()
-
+        #print(corrd_info)
         if corrd_info == 'nil':
             return
         else:
-            find_average(x, y, corrd_info[0], corrd_info[1], x_avg, y_avg)
+            find_average(x, corrd_info[0], x_avg)
+            find_average(y, corrd_info[1], y_avg)
+            print('x value:', x_avg[0]*0.625)
 
-            speed[0] = find_speed(y_avg)
-            speed[1] = find_speed(x_avg)
-            counter += 1
-            PDreg(x_avg, y_avg)
-            print('x=', x_avg)
-            print('y=', y_avg)
+            print('y value:', y_avg[1]*0.625)
             print()
+            speed[0] = find_speed(y_avg, 0)
+            speed[1] = find_speed(x_avg, 1)
+            PDreg(x_avg, y_avg)
+            #Preg(corrd_info[0], corrd_info[1])
 
+            #print('servo 1:', servo_values[1],'servo 2:', servo_values[2],'servo 3', servo_values[0])
             all_angle_assign(servo_values[0], servo_values[1], servo_values[2])
 
+
+
+
     def write_arduino(data):
+        #print('The angles send to the arduino : ', data)
+
         arduino.write(bytes(data, 'utf-8'))
 
     def write_servo():
@@ -186,7 +226,7 @@ def servo_control(key2, queue):
         elif ang3 < maxValue:
             ang3 = maxValue
 
-        angles: tuple = (round(ang1, 5),  # orginalt 1 komma
+        angles: tuple = (round(ang1, 5), #orginalt 1 komma
                          round(ang2, 5),
                          round(ang3, 5))
 
@@ -198,18 +238,18 @@ def servo_control(key2, queue):
 
         writeCoord()
 
-        wait = sample_time - (time_array[0] - time_array[1])
+        no_sleep -= 1 if no_sleep >= 0 else 0
+        wait = 0 if no_sleep != 0 else delta_t - ((time_array[0] - time_array[1]))
         #time.sleep(wait)
-
     root.mainloop()  # running loop
 
-
 if __name__ == '__main__':
-    queue = Queue()  # The queue is done inorder for the communication between the two processes.
-    key1 = 1  # just two dummy arguments passed for the processes
+
+    queue = Queue() # The queue is done inorder for the communication between the two processes.
+    key1 = 1 # just two dummy arguments passed for the processes
     key2 = 2
-    p1 = mp.Process(target=ball_track, args=(key1, queue))  # initiate ball tracking process
-    p2 = mp.Process(target=servo_control, args=(key2, queue))  # initiate servo controls
+    p1 = mp.Process(target= ball_track, args=(key1, queue)) # initiate ball tracking process
+    p2 = mp.Process(target=servo_control,args=(key2, queue)) # initiate servo controls
     p1.start()
     p2.start()
     p1.join()
